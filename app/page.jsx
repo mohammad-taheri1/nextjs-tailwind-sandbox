358c6eb4fbcd9bf4c01bf81bb6d92bc91e476a4b
MamadTaheri68 <mamad.taheri.68@gmail.com>
2023-05-26 13:34:43 +0430

    refact: reset entire project

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HomePage = () => {
+   return (
+      <div>
+         <h1>HomePage</h1>
+         <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ducimus natus ipsa quisquam provident aliquid
+            officia perferendis molestiae omnis, voluptate recusandae dolor, minus porro expedita incidunt amet
+            temporibus mollitia enim accusantium saepe? Ipsa dolorum accusantium voluptate saepe nobis esse, dolores
+            perferendis deleniti aliquid. Fugiat voluptatem dolorem aspernatur commodi repellat consequuntur.
+         </p>
+      </div>
+   );
+};
+
+export default HomePage;
