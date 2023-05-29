b90e8dd4f382944c21d3363e8276122ffe1ae97f
MamadTaheri68 <mamad.taheri.68@gmail.com>
2023-05-29 13:59:46 +0430

    feat: add LanguageSelectBox

--- components/layout/MainHeader.jsx
+++ components/layout/MainHeader.jsx
@@ -4,6 +4,7 @@ import { MenuContext } from "@/context/MenuContext";
 import React, { useContext } from "react";
 import { FaBars } from "react-icons/fa";
 import UserAreaSelectBox from "./UserAreaSelectBox";
+import LanguageSelectBox from "./LanguageSelectBox";
 
 const MainHeader = () => {
    const { toggle } = useContext(MenuContext);
@@ -12,6 +13,9 @@ const MainHeader = () => {
       <div className="bg-white flex justify-between items-center px-4 h-12 mb-4">
          <div>Brand</div>
          <div className="flex justify-center items-center gap-3">
+            <div>
+               <LanguageSelectBox />
+            </div>
             <div onClick={toggle} className="lg:hidden">
                <FaBars className="cursor-pointer" />
             </div>
