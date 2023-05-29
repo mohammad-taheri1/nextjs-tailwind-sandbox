72d30c5c234bc3c148d2d308c2b883ad0c4a3e67
MamadTaheri68 <mamad.taheri.68@gmail.com>
2023-05-29 13:47:13 +0430

    feat: add General NotFound page

---
+++ app/[...not-found]/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFoundPage = () => {
+   return (
+      <div className="bg-green-200">
+         <h1 className="text-green-500">NotFoundPage</h1>
+      </div>
+   );
+};
+
+export default NotFoundPage;
